bab254377a0dc2692c8fbda7d13f929eccdf0e3a
Brandon <Blent1050@yahoo.com>
2019-01-25 16:35:37 -0500

    added deployment

--- server.js
+++ server.js
@@ -81,7 +81,15 @@ server.delete('/smurfs/:id', (req, res) => {
   }
 });
 
-server.listen(port, err => {
-  if (err) console.log(err);
-  console.log(`server is listening on port ${port}`);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("village/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "village", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("server listening on port 5000");
 });
